721d3f98f78ade9b6410df56be14dc78f384e3cd
ANANTAPUR SADIYA MUDASSIR <56169907+sadiya-08@users.noreply.github.com>
2021-06-15 04:42:59 +0530

    Update settings.py

--- Hello/settings.py
+++ Hello/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-$jnhsw=9q62af-69kxfn_shizcb&+%&)&33u-vs9k5om=(^45%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['proclubsrit.herokuapp.com', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['proclubsrit.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
