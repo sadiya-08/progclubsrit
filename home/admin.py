04c3b09dd68c0483cde91f5124111c444effe178
ANANTAPUR SADIYA MUDASSIR <56169907+sadiya-08@users.noreply.github.com>
2021-06-15 03:17:55 +0530

    Add files via upload

---
+++ home/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from home.models import Contact
+from home.models import Gallery
+
+
+# Register your models here.
+admin.site.register(Contact)
+admin.site.register(Gallery)
+
+
